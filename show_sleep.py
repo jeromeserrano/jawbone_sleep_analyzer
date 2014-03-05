2e77015dd5e50a3e55a76d76a095da403d1d6ae6
Diana Tatu <diana.tatu@gmail.com>
2014-03-05 15:54:17 +0200

    Add mthod for fetching moves data of the current user.

--- show_sleep.py
+++ show_sleep.py
@@ -18,6 +18,11 @@ USERNAME = ''
 PASSWORD = ''
 LOGIN_URL = 'https://jawbone.com/user/signin/login'
 SLEEP_DATA_URL = 'https://jawbone.com/nudge/api/v.1.33/users/@me/sleeps'
+MOVES_DATA_URL = 'https://jawbone.com/nudge/api/v.1.33/users/@me/moves'
+
+HIGH_STEPS_SCORE = 20000
+MEDIUM_STEPS_SCORE = 10000
+LOW_STEPS_SCORE = 2000
 
 
 class JawboneSleepAnalyzer():
@@ -51,14 +56,14 @@ class JawboneSleepAnalyzer():
             Returns the list of sleeps of the current user.
             Example:
                 {
-                   “meta”:
+                   "meta":
                    {
-                      “user_xid”: “6xl39CsoVp2KirfHwVq_Fx”,
-                      “message”: “OK”,
-                      “code”: 200
+                      "user_xid": "6xl39CsoVp2KirfHwVq_Fx",
+                      "message": "OK",
+                      "code": 200
                       "time": 1386122022
                    },
-                   “data”:
+                   "data":
                    {
                       "items":
                       [{
@@ -95,21 +100,14 @@ class JawboneSleepAnalyzer():
                       {
                          "next": "/nudge/api/v.1.0/users/6xl39CsoVp2KirfHwVq_Fx/sleeps?page_token=1384390680"
                       },
-                      “size”: 10
+                      "size": 10
                    }
                 }
 
         Throws:
             Exception if an error occured while making the sleep GET request.
         """
-        opener = urllib2.build_opener()
-        opener.addheaders.append(('x-nudge-token', token))
-        resp = opener.open(SLEEP_DATA_URL)
-        data = json.load(resp)
-        if 'error' in data:
-            raise Exception("Request to %r failed: %r" % (SLEEP_DATA_URL, data))
-
-        return data
+        return self._request(token, SLEEP_DATA_URL)
 
     def display_sleep_data(self, data):
         """ Display sleep data from Jawbone API.
@@ -124,8 +122,8 @@ class JawboneSleepAnalyzer():
         a white space has been introduced at midnight and noon.
         """
         sleep_data = []
-        for i in range(0, len(data["data"]["items"])):
-            details = data["data"]["items"][i]["details"]
+        for item in data["data"]["items"]:
+            details = item["details"]
             asleep_dt = datetime.fromtimestamp(details["asleep_time"])
             awake_dt = datetime.fromtimestamp(details["awake_time"])
             sleep_data.append([asleep_dt, awake_dt])
@@ -158,3 +156,128 @@ class JawboneSleepAnalyzer():
             print line
             if (d.weekday() == 6):
                 print ""
+
+    def get_moves_data(self, token):
+        """ Perform GET request to obtain moves data.
+
+        Returns:
+            Returns the list of moves of the current user.
+            Example:
+                {
+                   "meta":
+                   {
+                      "user_xid": "6xl39CsoVp2KirfHwVq_Fx",
+                      "message": "OK",
+                      "code": 200
+                      "time": 1386122022
+                   },
+                   "data":
+                   {
+                      "items":
+                      [{
+                         "xid": "40F7_htRRnQwoMjIFucJ2g",
+                         "title": "16,804 steps",
+                         "type": "move",
+                         "time_created": 1384963500,
+                         "time_updated": 1385049599,
+                         "time_completed": 1385099220,
+                         "date": 20131121
+                         "snapshot_image": "/nudge/image/e/1385107737/40F7_htRRnQwoMjIFucJ2g/grEGutn_XYZ.png"
+                         "details":
+                         {
+                            "distance": 14745,
+                            "km": 14.745,
+                            "steps": 16804,
+                            "active_time": 11927,
+                            "longest_active": 2516,
+                            "inactive_time": 32760,
+                            "longest_idle": 27180,
+                            "calories": 1760.30480012,
+                            "bmr_day": 1697.47946931,
+                            "bmr": 1697.47946931,
+                            "bg_calories": 1099.9439497,
+                            "wo_calories": 388.506116077,
+                            "wo_time": 11484,
+                            "wo_active_time": 3902,
+                            "wo_count": 2,
+                            "wo_longest": 2516,
+                            "tz": "America/Los Angeles",
+                            "tzs":
+                            [
+                               [1384963500, "America/Phoenix"],
+                               [1385055720, "America/Los_Angeles"]
+                            ],
+                            "hourly_totals":
+                            {
+                                "2013112101":
+                                {
+                                    "distance": 1324,
+                                    "calories": 90.0120018125,
+                                    "steps": 1603,
+                                    "active_time": 793,
+                                    "inactive_time": 220,
+                                    "longest_active_time": 302,
+                                    "longest_idle_time": 780
+                                },
+                                "2013112101":
+                                {
+                                    "distance": 626,
+                                    "calories": 47.0120018125,
+                                    "steps": 455,
+                                    "active_time": 246,
+                                    "inactive_time": 260,
+                                    "longest_active_time": 203,
+                                    "longest_idle_time": 650
+                                },
+                                ... more hours ...
+                            }
+                         }
+                      },
+                      {
+                      ... more items ....
+                      }],
+                      "links":
+                      {
+                         "next": "/nudge/api/v.1.0/users/6xl39CsoVp2KirfHwVq_Fx/moves?page_token=1384390680"
+                      },
+                      "size": 10
+                   }
+                }
+
+        Throws:
+            Exception if an error occured while making the sleep GET request.
+        """
+        return self._request(token, MOVES_DATA_URL)
+
+    def display_moves_data(self, data):
+        from_date = min([item['time_completed'] for item in data["data"]["items"]])
+        to_date = max([item['time_completed'] for item in data["data"]["items"]])
+
+        print ("%s -> %s" %
+               (datetime.fromtimestamp(from_date).strftime('%Y-%m-%d'),
+                datetime.fromtimestamp(to_date).strftime('%Y-%m-%d')))
+
+        moves_data = []
+        line = ""
+
+        for item in data["data"]["items"]:
+            steps = item["details"]["steps"]
+            if steps > HIGH_STEPS_SCORE:
+                line += "|"
+            elif steps > MEDIUM_STEPS_SCORE:
+                line += ":"
+            elif steps > LOW_STEPS_SCORE:
+                line += "."
+            else:
+                line += " "
+        print line
+
+    def _request(self, token, url):
+        opener = urllib2.build_opener()
+        opener.addheaders.append(('x-nudge-token', token))
+        resp = opener.open(url)
+        data = json.load(resp)
+        if 'error' in data:
+            raise Exception("Request to %r failed: %r" % (url, data))
+
+        return data
